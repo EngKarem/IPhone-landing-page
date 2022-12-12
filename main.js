c672f701f17fff795746846c4d0451ab43bee261
Karem Mostafa <81931963+EngKarem@users.noreply.github.com>
2022-12-12 21:48:12 +0200

    Added Files

---
+++ main.js
@@ -0,0 +1,10 @@
+let image = document.querySelector(".img");
+    container = document.querySelector(".container");
+
+function phones(phone) {
+    image.src = phone;
+}
+
+function colors(color) {
+    container.style.backgroundColor=color;
+}
